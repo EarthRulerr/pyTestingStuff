414f27908b9400dd452d8b8034e506eeb1c4cfaf
Earth <85964403+EarthRulerr@users.noreply.github.com>
2022-11-09 09:57:04 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+multiline comment are
+c - o - o - l
+"""
+1+1 #no way
+1-1
+10*10
+10/10
+1==1 #is positive
+2==1 #is false
+1!=1 #not equal
+1 < 10 #10 is more than one // true
+1 > 10 #10 is less than one // false
+print("Hello, " + "world!") #joining
+"Hello, " + "world!"
+len("Hello, world!") #num of characters
+print("Hello, World! is ")
+len("Hello, World!") #num of chars
+name = "earthrulerr"
+input_string_name = input("Name: ")
+print(name)
